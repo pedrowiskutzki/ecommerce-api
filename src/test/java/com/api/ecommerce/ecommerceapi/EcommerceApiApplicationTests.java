ed4189b7c3ca8004c54bbba43fcb714d96cf6a71
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2022-10-04 16:18:10 -0300

    feat(Configuration):First commit

---
+++ src/test/java/com/api/ecommerce/ecommerceapi/EcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ecommerce.ecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
