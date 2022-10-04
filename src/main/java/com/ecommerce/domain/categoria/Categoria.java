42855a9d52394f25c32fa8c63a0d0b890f0cd685
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2022-10-04 19:23:13 -0300

    feat(Categoria):Inicio do crud de categoria

---
+++ src/main/java/com/ecommerce/domain/categoria/Categoria.java
@@ -0,0 +1,28 @@
+package com.ecommerce.domain.categoria;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Categoria {
+
+  @EqualsAndHashCode.Include
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String nome;
+
+  @Column(nullable = false)
+  private String descricao;
+
+}
