9a08e022389fa9531aa33236d434c935457b689e
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2022-10-18 15:07:47 -0300

    feat(Migration): Arquivos de migração

---
+++ src/main/resources/db/migration/V001__tab-endereco-cliente-pedido.sql
@@ -0,0 +1,25 @@
+CREATE TABLE endereco (id_endereco SERIAL PRIMARY KEY, 
+cep varchar(9) NOT NULL, 
+rua varchar(100) NOT NULL,
+bairro varchar(50) NOT NULL,
+cidade varchar(30),
+numero INTEGER NOT NULL,
+complemento varchar(20),
+estado varchar(2));
+
+CREATE TABLE cliente (id_cliente SERIAL PRIMARY KEY,
+email varchar(30) NOT NULL,
+nome_usuario varchar(20) NOT NULL,
+nome_completo varchar(60) NOT NULL,
+senha varchar(255),
+cpf varchar(14) NOT NULL,
+telefone varchar(11),
+data_nasc DATE, 
+id_endereco INTEGER, FOREIGN KEY(id_endereco) REFERENCES endereco(id_endereco));
+
+CREATE TABLE pedido (id_pedido SERIAL PRIMARY KEY,
+data_pedido DATE NOT NULL,
+data_entrega DATE, 
+data_envio DATE,
+status VARCHAR(20),
+id_cliente INTEGER, FOREIGN KEY(id_cliente) REFERENCES cliente(id_cliente));
