162f55ad0084e891b4cc6fc63ac80cfe8e0c30e7
MatheusTavares94 <mmatheus_ttavares@hotmail.com>
2022-10-22 16:29:52 -0300

    feat(Model):Update

--- src/main/java/com/ecommerce/domain/model/Produto.java
+++ src/main/java/com/ecommerce/domain/model/Produto.java
@@ -1,7 +1,10 @@
 package com.ecommerce.domain.model;
 
+<<<<<<< HEAD
 
 import java.math.BigDecimal;
+=======
+>>>>>>> 471787e (feat(Model):Update)
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -47,6 +50,26 @@ public class Produto {
   //imagem
   //id_categoria
   
+<<<<<<< HEAD
+=======
+    @Column(name = "nome",nullable = false)
+    private String nome;
+  
+    @Column(name = "descricao", nullable = false)
+    private String descricao;
+  
+    @Column(name = "qtd_estoque", nullable = false)
+    private Long quantidadeEstoque;
+
+    @Column(name = "data_cadastro")
+    private Date dataCadastro;
+
+    @Column(name = "valor_unitario", nullable = false)
+    private Double valorUnitario;
+
+	// imagem
+
+>>>>>>> 471787e (feat(Model):Update)
 	@ManyToOne
 	@JoinColumn(name = "id_categoria")
 	private Categoria categoria;
@@ -54,4 +77,73 @@ public class Produto {
 	@OneToOne
 	private ItemPedido itemPedido;
 
+<<<<<<< HEAD
+=======
+	public Long getId_produto() {
+		return id_produto;
+	}
+
+	public void setId_produto(Long id_produto) {
+		this.id_produto = id_produto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Long getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Long quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Double getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(Double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public ItemPedido getItemPedido() {
+		return itemPedido;
+	}
+
+	public void setItemPedido(ItemPedido itemPedido) {
+		this.itemPedido = itemPedido;
+	}
+
+	
+
+>>>>>>> 471787e (feat(Model):Update)
 }
